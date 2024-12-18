52c0e7e2e8376a02f118073cb2584f9ea1090d74
Jack Yao <magicyao22@icloud.com>
2024-12-18 12:09:48 +0800

    Completed 2024 Week 15.

--- scatter_plots/pass_rush_plot.py
+++ scatter_plots/pass_rush_plot.py
@@ -138,7 +138,7 @@ if __name__ == "__main__":
     from config import HAVOC_NOTE
 
     queries = [
-        [2024, "DI", 150], [2024, "ED", 170]
+        [2024, "DI", 150], [2024, "ED", 190]
     ]
     for query_season, query_position, query_threshold in queries:
         plot_pass_rush(
